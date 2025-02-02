3fe946c2517d0c80f93ded10144d5c610ab3d1b0
Suhail Moeen <suhailmmoeen@gmail.com>
2025-02-02 11:04:17 -0500

    Chore: Motor and Servo tests

--- utrahacks2025/utrahacks2025.ino
+++ utrahacks2025/utrahacks2025.ino
@@ -1,11 +1,25 @@
+#include <Servo.h>
+#include <L298NX2.h>
+
 const int trigPin = 12;
 const int echoPin = 13;
+const int servoPin = 11;
+const int EN1pin = 2;
+const int EN2pin = 3;
+const int N1pin = 6;
+const int N2pin = 9;
+const int N3pin = 5;
+const int N4pin = 4;
 long duration;
 int distance;
+
+Servo myServo;
+L298NX2 motors(EN1pin, N1pin, N2pin, EN2pin, N3pin, N4pin);
 
 void setup() {
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
+  myServo.attach(servoPin);
   Serial.begin(9600);
 }
 
@@ -18,5 +32,21 @@ void loop() {
   duration = pulseIn(echoPin, HIGH);
   distance = duration * 0.034 / 2;
   Serial.println(distance);
+
+  myServo.write(0);
+  delay(1000);
+  myServo.write(180);
+  delay(1000);
+  myServo.write(0);
+  delay(1000);  
+
+  motors.forwardA();
+  delay(1000);
+  motors.stopA();
+  delay(1000);
+  motors.forwardB();
+  delay(1000);
+  motors.stopB();
+  delay(1000);
 }
 
